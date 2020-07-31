345c2bddf619437fa16e512e9d131c1555e667d9
AlexandrPashanov <kaaronar@mail.ru>
2020-07-31 23:14:36 +0300

    4 files modified

--- app/tabs/tab-component/tab.component.ts
+++ app/tabs/tab-component/tab.component.ts
@@ -1,9 +1,10 @@
 import {
   ChangeDetectionStrategy, ChangeDetectorRef,
-  Component, OnDestroy, OnInit
+  Component, ContentChild, OnDestroy, OnInit, TemplateRef, ViewChild
 } from '@angular/core';
 import {ActiveTabService} from '../../service/active-tab.service';
 import {Subscription} from 'rxjs';
+import TabContentComponent from './tab-elements/tab-content.component';
 
 @Component({
   selector: 'tab',
@@ -11,13 +12,13 @@ import {Subscription} from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class TabComponent implements OnInit, OnDestroy{
-
+  @ViewChild('tabContentTemplate', {static: true}) tabContent: TemplateRef<TabContentComponent>;
   constructor(private activeTabService: ActiveTabService, private cdr: ChangeDetectorRef){  }
 
   isActiveTab: boolean = false;
   idNumber: number;
 
-  activeIndexSubscription : Subscription;
+  activeIndexSubscription: Subscription;
 
   click() {
     this.activeTabService.setCurrentActiveIndex(this.idNumber);
--- app/tabs/tabs-component/tabs.component.ts
+++ app/tabs/tabs-component/tabs.component.ts
@@ -6,47 +6,58 @@ import {
   ContentChildren, OnDestroy,
   OnInit,
   QueryList,
-ViewChild,
-TemplateRef
+  ViewChild,
+  TemplateRef, ContentChild
 } from '@angular/core';
 import TabComponent from '../tab-component/tab.component';
 import {ActiveTabService} from '../../service/active-tab.service';
 import {Subscription} from 'rxjs';
+import TabTitleComponent from '../tab-component/tab-elements/tab-title.component';
+import TabContentComponent from '../tab-component/tab-elements/tab-content.component';
 
 @Component({
-    selector: 'tabs',
-    templateUrl: 'tabs.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'tabs',
+  templateUrl: 'tabs.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class TabsComponent implements AfterContentInit, OnInit, OnDestroy{
+export default class TabsComponent implements AfterContentInit, OnInit, OnDestroy {
   @ContentChildren(TabComponent) tabsList: QueryList<TabComponent>;
+  private tabsListArray: TabComponent[] = [];
   private currentActiveId = 0;
-  private tabListChangesSubscription : Subscription;
+  private tabListChangesSubscription: Subscription;
+  public currentComponentTemplate: TemplateRef<TabContentComponent>;
 
   constructor(private activeTabService: ActiveTabService, private cdr: ChangeDetectorRef) {
     this.activeTabService.activeIndex$.subscribe((activeIndex => {
       this.currentActiveId = activeIndex;
-    }))
+      if (this.tabsListArray.length > 0) {
+        this.currentComponentTemplate = this.tabsListArray[this.currentActiveId].tabContent;
+        this.cdr.detectChanges();
+      }
+    }));
   }
 
   ngAfterContentInit(): void {
     this.setTabs();
     this.activeTabService.setCurrentActiveIndex(this.currentActiveId);
+    this.tabsListArray = this.tabsList.toArray();
+    this.currentComponentTemplate = this.tabsListArray[this.currentActiveId].tabContent;
     this.tabListChangesSubscription = this.tabsList.changes.subscribe(tabs => {
-      this.tabsList = tabs.toArray();
+      this.tabsListArray = tabs.toArray();
       this.setTabs();
-      const checkIndex = this.tabsList ? this.tabsList.length - 1 : 0;
+      const checkIndex = this.tabsListArray ? this.tabsListArray.length - 1 : 0;
       if (this.currentActiveId > checkIndex) {
         this.activeTabService.setCurrentActiveIndex(checkIndex);
       }
       this.cdr.detectChanges();
     });
+    this.cdr.detectChanges();
   }
 
   setTabs() {
     this.tabsList.forEach((tab, index) => {
       tab.idNumber = index;
-    })
+    });
   }
 
   ngOnInit(): void {
